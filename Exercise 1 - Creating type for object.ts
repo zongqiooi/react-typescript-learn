dcf388d28acaa45a5da77ca6351443576c7e1c06
Zong Qi <huro710@hotmail.com>
2023-10-25 22:24:58 +0800

    feat: complete exercise 1

---
+++ Exercise 1 - Creating type for object.ts
@@ -0,0 +1,15 @@
+// Challenge: create a type describing this object
+
+type Product = {
+  name: string;
+  price: number;
+  images: string[];
+  status: string;
+};
+
+const product = {
+  name: "Shampoo",
+  price: 2.99,
+  images: ["image-1.png", "image-2.png"],
+  status: "published",
+};
